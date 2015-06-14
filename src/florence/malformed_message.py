1be49e1215969bfca4c53aa896834552c3f978f8
Sriram Natarajan <natarajan.sriram@gmail.com>
2015-06-14 02:09:30 +0000

    Flake8 Malformed Message file
    Signed-off-by: Sriram Natarajan <natarajan.sriram@gmail.com>

--- src/florence/malformed_message.py
+++ src/florence/malformed_message.py
@@ -3,27 +3,24 @@ Check Malformed OpenFlow control message
 """
 
 import struct
-import ofp
-import loxi.of13.action as action
+
 
 class malformed_message(object):
-    #version = ofp.OFP_VERSION
-    #type = 217
 
     def __init__(self, version=None, type=None, length=None, xid=None):
         self.version = version
         self.type = type
         self.xid = xid
-	self.length = length
+        self.length = length
 
     def pack(self):
         packed = []
         packed.append(struct.pack("!B", self.version))
         packed.append(struct.pack("!B", self.type))
-        packed.append(struct.pack("!H", 0)) # placeholder for length at index 2
+        packed.append(struct.pack("!H", 0))  # placeholder for length
         packed.append(struct.pack("!L", self.xid))
         if self.length is None:
-	    self.length = sum([len(x) for x in packed])
+            self.length = sum([len(x) for x in packed])
         packed[2] = struct.pack("!H", self.length)
         return ''.join(packed)
 
@@ -32,11 +29,16 @@ class malformed_message(object):
         raise NotImplementedError()
 
     def __eq__(self, other):
-        if type(self) != type(other): return False
-        if self.version != other.version: return False
-        if self.type != other.type: return False
-        if self.xid != other.xid: return False
-	if self.length != other.length: return False
+        if type(self) != type(other):
+            return False
+        if self.version != other.version:
+            return False
+        if self.type != other.type:
+            return False
+        if self.xid != other.xid:
+            return False
+        if self.length != other.length:
+            return False
         return True
 
     def __ne__(self, other):
