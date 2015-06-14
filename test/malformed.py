58139af3f941e49157ccb7b60a89e2fcb98496b2
Sriram Natarajan <natarajan.sriram@gmail.com>
2015-06-14 03:35:03 +0000

    Flake8 malformed tests
    Signed-off-by: Sriram Natarajan <natarajan.sriram@gmail.com>

--- test/malformed.py
+++ test/malformed.py
@@ -4,14 +4,11 @@ Malformed control message test cases
 """
 
 import logging
-
-from florence import config
 import oftest.base_tests as base_tests
 import ofp
-
 import florence.malformed_message as malformed_message
-
 from oftest.testutils import *
+
 
 class UnsupportedVersion(base_tests.SimpleProtocol):
     """
@@ -33,6 +30,7 @@ class UnsupportedVersion(base_tests.SimpleProtocol):
         self.assertTrue(reply.code == ofp.OFPET_HELLO_FAILED,
                         "reply error code is not bad type")
 
+
 class UnsupportedMessageType(base_tests.SimpleProtocol):
     """
     Send a message with a bad type and verify an error is returned
@@ -44,7 +42,8 @@ class UnsupportedMessageType(base_tests.SimpleProtocol):
 
         reply, pkt = self.controller.transact(request)
         logging.info(repr(pkt))
-        self.assertTrue(reply is not None, "No response to malformed message type")
+        self.assertTrue(reply is not None,
+                        "No response to malformed message type")
         self.assertTrue(reply.type == ofp.OFPT_ERROR,
                         "reply not an error message")
         logging.info(reply.err_type)
@@ -52,6 +51,7 @@ class UnsupportedMessageType(base_tests.SimpleProtocol):
                         "reply error type is not bad request")
         self.assertTrue(reply.code == ofp.OFPBRC_BAD_TYPE,
                         "reply error code is not bad type")
+
 
 class Version(base_tests.SimpleProtocol):
     """
@@ -64,7 +64,8 @@ class Version(base_tests.SimpleProtocol):
 
         reply, pkt = self.controller.transact(request)
         logging.info(repr(pkt))
-        self.assertTrue(reply is not None, "No response to malformed message version")
+        self.assertTrue(reply is not None,
+                        "No response to malformed message version")
         self.assertTrue(reply.type == ofp.OFPT_ERROR,
                         "reply not an error message")
         logging.info(reply.err_type)
@@ -72,6 +73,7 @@ class Version(base_tests.SimpleProtocol):
                         "reply error type is not bad request")
         self.assertTrue(reply.code == ofp.OFPBRC_BAD_VERSION,
                         "reply error code is not bad type")
+
 
 class ControlMessageType(base_tests.SimpleDataPlane):
     """
@@ -85,21 +87,18 @@ class ControlMessageType(base_tests.SimpleDataPlane):
         match = ofp.match([
             ofp.oxm.in_port(in_port),
         ])
-        priority = 10
-
+        inst = ofp.instruction.apply_actions([ofp.action.output(out_port1)])
         logging.info("Inserting flow with malformed control message type")
         # Wrong type in flow mod
-	request = ofp.message.flow_add(
-                table_id=10,
-		type=16,
-                match=match,
-                instructions=[
-                    ofp.instruction.apply_actions([ofp.action.output(out_port1)]),
-                ],
-                hard_timeout=1000)
+        request = ofp.message.flow_add(table_id=10,
+                                       type=16,
+                                       match=match,
+                                       instructions=[inst],
+                                       hard_timeout=1000)
         reply, pkt = self.controller.transact(request)
 
-        self.assertTrue(reply is not None, "No response to malformed control message type")
+        self.assertTrue(reply is not None,
+                        "No response to malformed control message type")
         self.assertTrue(reply.type == ofp.OFPT_ERROR,
                         "reply not an error message")
         self.assertTrue(reply.err_type == ofp.OFPET_BAD_REQUEST,
@@ -107,9 +106,10 @@ class ControlMessageType(base_tests.SimpleDataPlane):
         self.assertTrue(reply.code == ofp.OFPFMFC_BAD_COMMAND,
                         "reply error code is not bad timeout")
 
+
 class MatchLength(base_tests.SimpleDataPlane):
     """
-    Verify malformed message length 
+    Verify malformed message length
     """
     def runTest(self):
         in_port, out_port1 = openflow_ports(2)
@@ -119,18 +119,15 @@ class MatchLength(base_tests.SimpleDataPlane):
         match = ofp.match([
             ofp.oxm.in_port(in_port),
         ])
-        priority = 10
-
+        action = ofp.action.output(out_port1)
+        inst = ofp.instruction.apply_actions(actions=[action])
         logging.info("Inserting flow with bad match length")
         # Wrong length in flow mod
-        request = ofp.message.flow_add(
-                table_id=10,
-                match=match,
-                instructions=[
-                    ofp.instruction.apply_actions(actions=[ofp.action.output(out_port1)]),
-                ],
-                hard_timeout=1000,
-                length=10000)
+        request = ofp.message.flow_add(table_id=10,
+                                       match=match,
+                                       instructions=[inst],
+                                       hard_timeout=1000,
+                                       length=10000)
         reply, pkt = self.controller.transact(request)
 
         self.assertTrue(reply is not None, "No response to bad match length")
@@ -141,6 +138,7 @@ class MatchLength(base_tests.SimpleDataPlane):
         self.assertTrue(reply.code == ofp.OFPBMC_BAD_LEN,
                         "reply error code is not bad match length")
 
+
 class MatchValue(base_tests.SimpleDataPlane):
     """
     Verify malformed message value
@@ -150,22 +148,17 @@ class MatchValue(base_tests.SimpleDataPlane):
 
         delete_all_flows(self.controller)
 
-        match = ofp.match([
-            ofp.oxm.in_port(in_port),
-	    ofp.oxm.eth_type(0x800),
-	    ofp.oxm.ip_dscp(100),
-        ])
-        priority = 10
-
+        match = ofp.match([ofp.oxm.in_port(in_port),
+                          ofp.oxm.eth_type(0x800),
+                          ofp.oxm.ip_dscp(100), ])
+        action = ofp.action.output(out_port1)
+        inst = ofp.instruction.apply_actions(actions=[action])
         logging.info("Inserting flow with bad match value")
         # Wrong match value in flow mod
-        request = ofp.message.flow_add(
-                table_id=10,
-                match=match,
-                instructions=[
-                    ofp.instruction.apply_actions(actions=[ofp.action.output(out_port1)]),
-                ],
-                hard_timeout=1000,)
+        request = ofp.message.flow_add(table_id=10,
+                                       match=match,
+                                       instructions=[inst],
+                                       hard_timeout=1000,)
         reply, pkt = self.controller.transact(request)
 
         self.assertTrue(reply is not None, "No response to bad match value")
@@ -176,20 +169,22 @@ class MatchValue(base_tests.SimpleDataPlane):
         self.assertTrue(reply.code == ofp.OFPBMC_BAD_VALUE,
                         "reply error code is not bad match value")
 
+
 class IncompatibleHello(base_tests.SimpleProtocol):
     """
-    Send an incompatible hello error message after connection establishment to monitor switch behavior
+    Send an incompatible hello error message after
+    connection establishment to monitor switch behavior
     """
 
     def runTest(self):
         logging.info("Running " + str(self))
         request = ofp.message.hello_failed_error_msg(code=0)
         reply, pkt = self.controller.transact(request)
-	self.assertTrue(reply is not None, "No response to incompatible hello")
+        self.assertTrue(reply is not None, "No response to incompatible hello")
         self.assertTrue(reply.type == ofp.OFPT_ERROR,
                         "reply not an error message")
         logging.info(reply.err_type)
-	logging.info(reply.code)
+        logging.info(reply.code)
         self.assertTrue(reply.err_type == ofp.OFPET_HELLO_FAILED,
                         "reply error type is not hello failed")
         self.assertTrue(reply.code == ofp.OFPHFC_INCOMPATIBLE,
