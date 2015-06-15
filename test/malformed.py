0f18ebfba8c8f91bc2a8c04b28adf112f86e53e3
Sriram Natarajan <natarajan.sriram@gmail.com>
2015-06-15 23:22:43 +0000

    Fix Incompatible Hello handler test Add Malformed Cookie Value test Add Bad Buffer ID value test
    Signed-off-by: Sriram Natarajan <natarajan.sriram@gmail.com>

--- test/malformed.py
+++ test/malformed.py
@@ -4,6 +4,7 @@ Malformed control message test cases
 """
 
 import logging
+from florence import config
 import oftest.base_tests as base_tests
 import ofp
 import florence.malformed_message as malformed_message
@@ -164,7 +165,7 @@ class MatchValue(base_tests.SimpleDataPlane):
                         "reply error code is not bad match value")
 
 
-class IncompatibleHello(base_tests.SimpleProtocol):
+class IncompatibleHello(base_tests.Handshake):
     """
     Send an incompatible hello error message after
     connection establishment to monitor switch behavior
@@ -172,12 +173,73 @@ class IncompatibleHello(base_tests.SimpleProtocol):
 
     def runTest(self):
         logging.info("Running " + str(self))
+        self.controllerSetup(config["controller_host"],
+                             config["controller_port"])
+        self.controllers[0].connect(self.default_timeout)
+
+        logging.info("Connected to switch" +
+                     str(self.controllers[0].switch_addr))
+        self.controllers[0].message_send(ofp.message.hello())
         request = ofp.message.hello_failed_error_msg(code=0)
+        reply, pkt = self.controllers[0].transact(request,
+                                                  self.default_timeout)
+        self.assertTrue(reply is None, """Response received for incompatible """  
+                        """hello. Requested not rejected or connection not closed""")
+
+
+class CookieValue(base_tests.SimpleDataPlane):
+    """
+    Verify flow modification fails with bad cookie value
+    Checking with a reserved value which is expected to fail.
+    """
+    def runTest(self):
+        in_port, out_port1 = openflow_ports(2)
+        match = ofp.match([
+            ofp.oxm.in_port(in_port),
+        ])
+        inst = ofp.instruction.apply_actions([ofp.action.output(out_port1)])
+        logging.info("Inserting flow with malformed/reserved cookie value")
+        # Wrong type in flow mod
+        request = ofp.message.flow_add(table_id=1,
+                                       match=match,
+                                       instructions=[inst],
+                                       buffer_id=ofp.OFP_NO_BUFFER,
+                                       cookie=0xfffffffffffffff,)
         reply, pkt = self.controller.transact(request)
-        self.assertTrue(reply is not None, "No response to incompatible hello")
+
+        self.assertTrue(reply is not None,
+                        "No response to malformed cookie value")
         self.assertTrue(reply.type == ofp.OFPT_ERROR,
                         "reply not an error message")
-        self.assertTrue(reply.err_type == ofp.OFPET_HELLO_FAILED,
-                        "reply error type is not hello failed")
-        self.assertTrue(reply.code == ofp.OFPHFC_INCOMPATIBLE,
-                        "reply error code is not hello incompatible")
+        self.assertTrue(reply.err_type == ofp.OFPET_FLOW_MOD_FAILED,
+                        "reply error type is not flow mod failed")
+        self.assertTrue(reply.code == ofp.OFPFMFC_UNKNOWN,
+                        "reply error code is not unknown code type")
+
+
+class BufferID(base_tests.SimpleDataPlane):
+    """
+    Verify flow modification fails with bad buffer_id value
+    """
+    def runTest(self):
+        in_port, out_port1 = openflow_ports(2)
+        match = ofp.match([
+            ofp.oxm.in_port(in_port),
+        ])
+        inst = ofp.instruction.apply_actions([ofp.action.output(out_port1)])
+        logging.info("Inserting flow with malformed/reserved buffered value")
+        # Wrong type in flow mod
+        request = ofp.message.flow_add(table_id=1,
+                                       match=match,
+                                       instructions=[inst],
+                                       buffer_id=1243,)
+        reply, pkt = self.controller.transact(request)
+
+        self.assertTrue(reply is not None,
+                        "No response to malformed cookie value")
+        self.assertTrue(reply.type == ofp.OFPT_ERROR,
+                        "reply not an error message")
+        self.assertTrue(reply.err_type == ofp. OFPET_BAD_REQUEST,
+                        "reply error type is not bad request")
+        self.assertTrue(reply.code == ofp.OFPBRC_BUFFER_UNKNOWN,	
+                        "reply error code is not unknown buffer code")
